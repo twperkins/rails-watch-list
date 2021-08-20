4e169143f00998e8bca0a59fd7626f48c06f86a4
Tom Perkins <twperkins.work@gmail.com>
2021-08-20 12:48:48 +0100

    sorted out home root path

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
 require 'json'
 require 'open-uri'
 
-if Rails.env.development?
+# if Rails.env.development?
   puts "Destroying lists..."
   List.destroy_all
   puts "Destroying bookmarks..."
   Bookmark.destroy_all
   puts "Destroying movies..."
   Movie.destroy_all
-end
+# end
 
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-serial = URI.open(url).read
-movies = JSON.parse(serial)["results"]
+10.times do |i|
+  url = "http://tmdb.lewagon.com/movie/top_rated?page=#{i + 1}"
+  serial = URI.open(url).read
+  movies = JSON.parse(serial)["results"]
 
-movies.each do |result|
-  if result["original_language"] == "en"
-    Movie.create(
-      title: result["original_title"],
-      overview: result["overview"],
-      poster_url: "https://image.tmdb.org/t/p/original#{result["poster_path"]}",
-      rating: result["vote_average"]
-    )
-    puts "added #{result["original_title"]}"
+  movies.each do |result|
+    if result["original_language"] == "en"
+      Movie.create(
+        title: result["original_title"],
+        overview: result["overview"],
+        poster_url: "https://image.tmdb.org/t/p/original#{result["poster_path"]}",
+        rating: result["vote_average"]
+      )
+      puts "added #{result["original_title"]}"
+    end
   end
 end
 
