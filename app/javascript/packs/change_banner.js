0a0bcebe804c50cba190599db90e3eaa5d82933d
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2021-11-16 17:29:57 +0000

    tweaked filter

--- app/javascript/packs/change_banner.js
+++ app/javascript/packs/change_banner.js
@@ -12,7 +12,7 @@ const changeBanner = () => {
   categories.forEach((category) => {
     category.addEventListener("mouseover", () => {
       console.log(photos[category.querySelector("a").innerText])
-      mainBanner.style = `background-image: linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url("${photos[category.querySelector("a").innerText]}")`
+      mainBanner.style = `background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("${photos[category.querySelector("a").innerText]}")`
     });
   });
   categories.forEach((category) => {
