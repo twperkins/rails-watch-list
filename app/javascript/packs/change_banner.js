39023d7b47bfe15fc6687b8afb7a754e14f242ea
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2021-11-16 17:52:48 +0000

    images now pulling correctly

--- app/javascript/packs/change_banner.js
+++ app/javascript/packs/change_banner.js
@@ -1,18 +1,10 @@
 const changeBanner = () => {
   const mainBanner = document.querySelector(".header");
   const categories = document.querySelectorAll(".category-card")
-  const photos = {}
-
-  categories.forEach((category) => {
-    photos[category.querySelector("a").innerText] = `https://source.unsplash.com/1600x900/?${category.querySelector("a").innerText}`
-  })
-
-  console.log(photos)
 
   categories.forEach((category) => {
     category.addEventListener("mouseover", () => {
-      console.log(photos[category.querySelector("a").innerText])
-      mainBanner.style = `background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("${photos[category.querySelector("a").innerText]}")`
+      mainBanner.style.backgroundImage = category.style.backgroundImage
     });
   });
   categories.forEach((category) => {
